9bf83f77523cf83bbb8bcc00212cb68be4aa8323
Simon Peter Debbarma <31207418+simonpeterdebbarma@users.noreply.github.com>
2019-12-01 11:00:00 -0600

    update README.md, and build temporary page

--- src/App.js
+++ src/App.js
@@ -6,18 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://media.giphy.com/media/bFTSLXI4Of15u/giphy.gif" />
+        <hr/>
+        Development in Progress
+        <p>Find me on Keybase - @simonpd</p>
       </header>
     </div>
   );
