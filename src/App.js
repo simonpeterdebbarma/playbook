44f6cd68e113b0ba68ec23914655f3ef401952ba
Simon Peter Debbarma <31207418+simonpeterdebbarma@users.noreply.github.com>
2019-12-01 11:07:33 -0600

    update README.md, and build temporary page

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://media.giphy.com/media/bFTSLXI4Of15u/giphy.gif" />
+        <banner>Development in Progress</banner>
         <hr/>
-        Development in Progress
+        <img src="https://media.giphy.com/media/26gstMoYksEdQRRC0/giphy.gif" />
+        <hr/>
         <p>Find me on Keybase - @simonpd</p>
       </header>
     </div>
