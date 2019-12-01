8074222e0a1bc50c58156451465fde8fa7fa80ee
Simon Peter Debbarma <31207418+simonpeterdebbarma@users.noreply.github.com>
2019-12-01 11:33:44 -0600

    Updates

---
+++ precache-manifest.8df7a98f07dd922a90a80d60d00bb787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadae3fbee93fa75562e00962d2ce5c4",
+    "url": "/playbook/index.html"
+  },
+  {
+    "revision": "4a50d8efda78e77b71b0",
+    "url": "/playbook/static/css/main.9651cb35.chunk.css"
+  },
+  {
+    "revision": "72b65802e9b2fb1a9037",
+    "url": "/playbook/static/js/2.3aadc6fe.chunk.js"
+  },
+  {
+    "revision": "4a50d8efda78e77b71b0",
+    "url": "/playbook/static/js/main.4e2c6ad4.chunk.js"
+  },
+  {
+    "revision": "4cf28529928750097959",
+    "url": "/playbook/static/js/runtime-main.8c378d92.js"
+  }
+]);
