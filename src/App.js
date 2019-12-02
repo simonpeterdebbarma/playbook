82da2ab362fcc815ce3f13a132b0064543ef7a73
Simon Peter Debbarma <31207418+simonpeterdebbarma@users.noreply.github.com>
2019-12-02 10:53:23 -0600

    add alt text to img

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <banner>Development in Progress</banner>
+        <hr/>
+        <img src="https://media.giphy.com/media/26gstMoYksEdQRRC0/giphy.gif" alt="I don't know what progress is because I don't measure time like you do"/>
+        <hr/>
+        <p>Find me on Keybase - @simonpd</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
